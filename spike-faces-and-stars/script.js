ae10e10e86155567592246233ecc648dfccff8b7
Duncan Mortimer <dmortime@thoughtworks.com>
2014-05-31 15:00:27 +0100

    Added counter to keep track of number of stars

--- spike-faces-and-stars/script.js
+++ spike-faces-and-stars/script.js
@@ -1,21 +1,32 @@
-
-function allowDrop(ev)
-{
-    ev.preventDefault();
-}
-
-function drag(ev)
-{
-    ev.dataTransfer.setData("Text",ev.target.id);
-}
-
-var medical_team = document.getElementsByClassName('medical-professional')
-    .map(function(e) { return e.id; });
-
-
+var allowDrop, drag, drop;
+var star_counter = {};
+window.onload = function () {
+    allowDrop = function (ev)
+    {
+        ev.preventDefault();
+    }
     
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("Text");
-    ev.target.appendChild(document.getElementById(data).cloneNode(true));
+    drag = function (ev)
+    {
+        ev.dataTransfer.setData("Text",ev.target.id);
+    }
+    
+    var medical_team = document.getElementsByClassName('medical-professional');
+    
+    var team_names = Array.prototype.map.call(
+        medical_team, 
+        function(e) {
+            star_counter[e.id] = 0; 
+            console.log(e.id);
+            return e.id;
+        });
+    
+    
+    drop = function(ev) {
+        star_counter[ev.target.id] += 1;
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("Text");
+        ev.target.appendChild(document.getElementById(data).cloneNode(true));
+        console.log(star_counter);
+    }
 }
